a933bf7e06bf1393d9f95f956e95592365d607eb
Utkarsh Sharma <flamingice2801@gmail.com>
2019-05-05 17:55:50 +0530

    Create quiz.cpp

---
+++ source/quiz.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <algorithm>
+#define QNO 30
+#define totques 10
+
+using namespace std;
+
+void Display()
+	{
+		cout<<"\n\n\n\n\n\n";
+		cout<<"\n\t   +-------+                          _______       _______                             ";
+		cout<<"\n\t   |       |      |         |            |                /                              ";
+		cout<<"\n\t   |       |      |         |            |               /                               ";
+		cout<<"\n\t   |       |      |         |            |              /                                ";
+		cout<<"\n\t   |    // |      |         |            |             /                                 ";
+		cout<<"\n\t   |   //  |      |         |            |            /                                  ";
+		cout<<"\n\t   +--//---+      |_________|         ___|___        /_______                          ";
+		cout<<"\n\n\n\n\n\n\t\t\t   PRESS ENTER TO PLAY     ";
+		cout<<"\n\n\n\t\t\t\t   RULES OF THE GAME   :     \n";
+		cout<<"\t\t\t\t   1) 4 MARKS WILL BE AWARDED FOR CORRECT     \n";
+		cout<<"\t\t\t\t   2) NO MARKS WILL AWARDED FOR INCORRECT     \n";
+		cout<<"\t\t\t\t   3) SCORE CARD WILL BE GIVEN AT END         \n";
+		system("pause");
+		system("cls");				
+	}
